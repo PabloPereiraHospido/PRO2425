fbb5ebf362f40038dbff6be0f4dca528250a38d1
Pablo 103 <Pablopereirahospido35@gmail.com>
2024-09-23 19:56:21 +0200

    practica de los literales e inicio del boletin1

--- PROGRAMACION/src/UD1/Boletin1.java
+++ PROGRAMACION/src/UD1/Boletin1.java
@@ -1,4 +1,37 @@
 package UD1;
 
 public class Boletin1 {
+    //ejercicio 1
+    /*línea 1 es el paquete en el que se estructura el código
+    línea tres inicio de comentario de bloque
+    línea 7 clase pública del .java
+    línea 9 definición de un elemento estático
+    línea 10 declaración de constante
+    línea 12 delaración de variable
+    línea 15 inicio de comentario en bloque
+    línea 22 comentario en linea explicativo del código
+     */
+
+    //ejercicio 2
+
+   /*
+   MARIA no es válido ya que no sigue las convenciones de escritura camel case
+   María no es válido por el acento
+   Salto_Mortal no es válido debido a la barra baja
+   "datos" no es válido ya que empieza por comillas
+   sueldo bruto no es válido debido al espacio
+   saltomortal no es valido ya que no respeta las convenciones de escritura high camel case
+   cu_1 no es válido por la barra baja
+   58 no es válido ya que comienza por un dígito
+   salto-mortal no es valido por la barra
+   if no es valido ya que es una palabra reservada del sistema
+   _&aprecio no es válido al empezar por la barra
+   año no es válido por la ñ
+    */
+    // ejercicio 3
+
+    /*
+    
+     */
+
 }
