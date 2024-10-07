35234c92143dff3d28ec43eba64683323e5de032
Pablo <Pablopereirahospido35@gmail.com>
2024-10-08 00:19:38 +0200

    primer ejercicio del boletin terminado

--- PROGRAMACION/src/UD2/Boletin21.java
+++ PROGRAMACION/src/UD2/Boletin21.java
@@ -1,7 +1,7 @@
 package UD2;
 
 public class Boletin21 {
-    public static void main (String [] ars){
+    public static void main(String[] ars) {
         System.out.println((true && true) || false == true);
         /*
          primero se ejecuta el true and true, lo que da true
@@ -29,10 +29,30 @@ y al no ser igual a true da false
         asi que sabiendo que el resutado final es false, se deja de hacer la ultima comprobacion
          */
         int ii = 3, jj = 2, kk = -1;
-        boolean resultado2 =ii == 3 || jj <= 2 && kk > 0;
+        boolean resultado2 = ii == 3 || jj <= 2 && kk > 0;
         System.out.println(resultado2);
-                
+        /*
+        primero comprueba que una de las dos primeras variables sean verdaderas,
+        y luego comprurba que la ultima variable es mayor o igual a cero, que lo es
+         */
+        int iii = 1;
+        boolean resultados3 = 3 == 2 || 5 > iii + iii;
+        System.out.println(resultados3);
+        /*
+        es verdadero ya que aunque la primera parte sea falsa,la segunda se vuelve verdadero al llegar
+        al 5 o superarlo,, lo cual ocurre, y con el or en medio en este caso es true
+         */
+        double x = 1, y = 4, z = 10, PI = 3.1416;
+        boolean resultado4 = PI * x * x > y || 2 * PI * x <= z;
+        boolean resultado5 = x > 3 && (y == 4 || x + y <= z);
+        System.out.println(resultado4);
+        System.out.println(resultado5);
+/*
+la primera de las dos es verdadera ya que aunque la primera parte da false, debido a que la segunda parte esta
+unido por un or y es verdadera, la expresion es verdadera
 
+por otro lado, el sgundo enunciado es falso por que todas sus condiciones son falsas
+ */
 
 
     }
