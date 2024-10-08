02883dac19520a3e26eb2d803804431116296638
Pablo 103 <Pablopereirahospido35@gmail.com>
2024-10-08 18:42:56 +0200

    el inicio de la Teoria del tema 2

--- PROGRAMACION/src/UD2/Teoria2.java
+++ PROGRAMACION/src/UD2/Teoria2.java
@@ -1,4 +1,13 @@
 package UD2;
 
-public class Teoria {
+public class Teoria2 {
+    public static void main (String [] ars){
+        boolean verdadero=true;
+        boolean falso=false;
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println(true && false);
+        System.out.println(true || false);
+
+    }
 }
