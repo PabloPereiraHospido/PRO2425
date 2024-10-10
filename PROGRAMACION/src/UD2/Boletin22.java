9ff9506e4a444c295929aac3c09203cb89189861
Pablo 103 <Pablopereirahospido35@gmail.com>
2024-10-10 19:53:36 +0200

    inicio del segundo boletin del tema 2

---
+++ PROGRAMACION/src/UD2/Boletin22.java
@@ -0,0 +1,31 @@
+package UD2;
+
+import java.util.Scanner;
+
+public class Boletin22 {
+    public static void main(String[] ars) {
+        //ejercicio 1 y 2
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("introduce un valor entre 0 y 10 (valores)");
+        int valores = scanner.nextInt();
+        if (valores <= 4) {
+
+            System.out.println("nota correcta,es un suspenso");
+        } else {
+            if (valores < 5) {
+
+
+                System.out.println("nota correcta,estas aprobado");
+            } else {
+                if (valores >= 11) {
+                    System.out.println("el valor introducido no es correcto");
+                }
+            }
+        }
+//ejercicio 3
+        Scanner scanner1 = new Scanner (System.in);
+        System.out.println("introduce un numero entero");
+        int numero= scanner1.nextInt();
+        if {
+    }
+}
--- PROGRAMACION/src/UD2/Teoria2.java
+++ PROGRAMACION/src/UD2/Teoria2.java
@@ -29,6 +29,31 @@ public class Teoria2 {
         } else {
             System.out.println("es menor de edad, su edad es " + edad2);
         }
+        //dia 10 de octubre
+        int edad3 = 24;
+        if (edad3 <= 17) {
+            if (edad3 < 3)
+                System.out.println("bebe");
+        } else {
+            if (edad3 >= 3 && edad3 <= 11) {
+                System.out.println("va a primaria");
+            } else {
+                if (edad3 >= 11 && edad3 <= 17) {
+                    System.out.println("va a secundaria");
+                } else {
+                    if (edad3 >= 17 && edad3 <= 35) {
+                        System.out.println("es joven");
+                    } else {
+                        if (edad3 >= 35 && edad3 <= 66) {
+                            System.out.println("es adulto");
+                        } else {
+                            if (edad3 >= 66 && edad3 <= 99) {
+                                System.out.println("es anciano");
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
-
 }
