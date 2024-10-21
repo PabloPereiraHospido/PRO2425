76698a596dee4cd86d6a7cfd2e87851d414647d6
Pablo 103 <Pablopereirahospido35@gmail.com>
2024-10-21 19:59:33 +0200

    empezamos a hacer los menus del boletin

--- PROGRAMACION/src/UD2/Boletin23.java
+++ PROGRAMACION/src/UD2/Boletin23.java
@@ -4,33 +4,38 @@ import java.util.Scanner;
 
 public class Boletin23 {
     public static void main(String[] args) {
+        System.out.println("elige un ejercicio : ");
+        Scanner in = new Scanner(System.in);
+        int ejercicio = in.nextInt();
+        switch (ejercicio) {
 
-        //ejercicio 1
-        int suma = 0;
+            case 1:
+                int suma = 0;
 
-        for (int n = 1; n <= 100; n++) {
-            suma += n;
+                for (int n = 1; n <= 100; n++) {
+                    suma += n;
+                }
+                System.out.println("La suma de los números del 1 al 100 es: " + suma);
+                break;
+            case 2:
+
+                Scanner scanner = new Scanner(System.in);
+                double suma1 = 0;
+
+                for (int i = 1; i <= 5; i++) {
+                    System.out.print("Ingrese el número " + i + ": ");
+                    double numero = scanner.nextDouble();
+                    suma1 += numero;
+                }
+
+                double media = suma1 / 5;
+
+                System.out.println("La media de los cinco números es: " + media);
+
+                scanner.close();
+                break;
+            //ejercicio 3
+
         }
-        System.out.println("La suma de los números del 1 al 100 es: " + suma);
-
-        //ejercicio 2
-
-        Scanner scanner = new Scanner(System.in);
-        double suma1 = 0;
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.print("Ingrese el número " + i + ": ");
-            double numero = scanner.nextDouble();
-            suma1 += numero;
-        }
-
-        double media = suma1 / 5;
-
-        System.out.println("La media de los cinco números es: " + media);
-
-        scanner.close();
-
-        //ejercicio 3
-
     }
 }
