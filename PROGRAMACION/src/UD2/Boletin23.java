da1769bbdcfab2b5b4596325d0e2b4450c25f7c3
Pablo 103 <Pablopereirahospido35@gmail.com>
2024-11-21 19:54:14 +0100

    terminado boletin 2.3 y empezado el tercer boletin

--- PROGRAMACION/src/UD2/Boletin23.java
+++ PROGRAMACION/src/UD2/Boletin23.java
@@ -5,204 +5,245 @@ import java.util.Scanner;
 public class Boletin23 {
     public static void main(String[] args) {
         System.out.println("introduce numero de ejercicio : ");
-        Scanner in = new Scanner(System.in);
-        int ejercicio = in.nextInt();
+        Scanner src = new Scanner(System.in);
+        int ejercicio = src.nextInt();
         switch (ejercicio) {
 
             case 1:
-                int suma = 0;
-
-                for (int n = 1; n <= 100; n++) {
-                    suma += n;
-                }
-                System.out.println("La suma de los números del 1 al 100 es: " + suma);
+                ejercicio1(src);
                 break;
             case 2:
-
-                Scanner scanner = new Scanner(System.in);
-                double suma1 = 0;
-
-                for (int i = 1; i <= 5; i++) {
-                    System.out.print("Ingrese el número " + i + ": ");
-                    double numero = scanner.nextDouble();
-                    suma1 += numero;
-                }
-
-                double media = suma1 / 5;
-
-                System.out.println("La media de los cinco números es: " + media);
-
-                scanner.close();
+                ejercicio2(src);
                 break;
             case 3:
-                Scanner scanner3 = new Scanner(System.in);
-                System.out.print("¿Cuántos números desea introducir? ");
-                int cantidadNumeros = scanner3.nextInt();
-                double suma3 = 0;
-
-                for (int i = 1; i <= cantidadNumeros; i++) {
-                    System.out.print("Ingrese el número " + i + ": ");
-                    double numero = scanner3.nextDouble();
-                    suma3 += numero;
-                }
-
-                double media3 = suma3 / cantidadNumeros;
-
-                System.out.println("La media de los " + cantidadNumeros + " números es: " + media3);
+                ejercicio3(src);
                 break;
             case 4:
-                Scanner scanner4 = new Scanner(System.in);
-                System.out.print("Ingrese el primer número: ");
-                int num1 = scanner4.nextInt();
-                System.out.print("Ingrese el segundo número: ");
-                int num2 = scanner4.nextInt();
-                int sumaPares = 0;
-
-                for (int i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
-                    if (i % 2 == 0) {
-                        sumaPares += i;
-                    }
-                }
-
-                System.out.println("La suma de los números pares entre " + num1 + " y " + num2 + " es: " + sumaPares);
+                ejercicio4(src);
                 break;
 
             case 5:
-                Scanner scanner5 = new Scanner(System.in);
-                System.out.print("Ingrese el número de términos: ");
-                int numTerms = scanner5.nextInt();
-                double sum = 0.0;
-
-                for (int k = 1; k <= numTerms; k++) {
-                    sum += 1.0 / k;
-                }
-
-                System.out.println("La suma de los " + numTerms + " términos es: " + sum);
+                ejercicio5(src);
                 break;
 
             case 6:
-                Scanner scanner6 = new Scanner(System.in);
-                System.out.print("Ingrese el número de términos: ");
-                int n = scanner6.nextInt();
-                double sumaSerie = 0.0;
-
-                for (int i = 1; i <= n; i++) {
-                    sumaSerie += (i * 1.0) / Math.pow(3, i);
-                }
-
-                System.out.println("La suma de los " + n + " términos es: " + sumaSerie);
+                ejercicio6(src);
                 break;
 
             case 7:
-                Scanner scanner7 = new Scanner(System.in);
-                System.out.print("Ingrese el número de términos: ");
-                int n7 = scanner7.nextInt();
-                double sumaSerie7 = 0.0;
-
-                for (int i = 1; i <= n7; i++) {
-                    sumaSerie7 += i / Math.pow(2, i);
-                }
-
-                System.out.println("La suma de los " + n7 + " términos es: " + sumaSerie7);
+                ejercicio7(src);
                 break;
             case 8:
-                Scanner scanner8 = new Scanner(System.in);
-                Scanner scanner81 = new Scanner(System.in);
-                System.out.println("Desde : ");
-                int Fahrenheit = scanner8.nextInt();
-                System.out.println("Hasta : ");
-                int Fahrenheit1 = scanner81.nextInt();
-                int Celsius = 0;
-                int Kelvin = 0;
-                int Rankine = 0;
-                if (Fahrenheit < Fahrenheit1)
-
-                    for (int i = Fahrenheit; i <= Fahrenheit1; i++) {
-                        Celsius = (i - 32) * 5 / 9;
-                        Kelvin = Celsius + 273;
-                        Rankine = i + 459;
-                        System.out.println("i: " + i + " Celsius: " + Celsius + " Kelvin: " + Kelvin + " Rankine: " + Rankine);
-                    }
-                else if (Fahrenheit > Fahrenheit1) {
-                    for (int i = Fahrenheit1; i <= Fahrenheit; i++) {
-                        Celsius = (i - 32) * 5 / 9;
-                        Kelvin = Celsius + 273;
-                        Rankine = i + 459;
-                        System.out.println("i: " + i + " Celsius: " + Celsius + " Kelvin: " + Kelvin + " Rankine: " + Rankine);
-                    }
-
-                }
-
-
+                ejercicio8(src);
                 break;
             case 9:
-                Scanner scanner9 = new Scanner(System.in);
-                System.out.print("Ingrese un número para calcular su factorial: ");
-                int numFactorial = scanner9.nextInt();
-
-                long factorial = 1;
-                for (int i = 1; i <= numFactorial; i++) {
-                    factorial *= i;
-                }
-                System.out.println("El factorial de " + numFactorial + " es: " + factorial);
-
-                System.out.println("Factoriales de los primeros 20 números naturales:");
-                for (int i = 0; i <= 20; i++) {
-                    long factorial20 = 1;
-                    for (int j = 1; j <= i; j++) {
-                        factorial20 *= j;
-                    }
-                    System.out.println(i + "! = " + factorial20);
-                }
+                ejercicio9(src);
                 break;
             case 10:
-                Scanner scanner10 = new Scanner(System.in);
-                System.out.print("Ingrese el número de términos: ");
-                int n10 = scanner10.nextInt();
-
-                int term = 1;
-                for (int i = 1; i <= n10; i++) {
-                    System.out.print(term + " ");
-                    term *= -2;
-                }
-                System.out.println();
+                ejercicio10(src);
                 break;
             case 11:
-                Scanner scanner11 = new Scanner(System.in);
-                System.out.print("Ingrese la cantidad de números: ");
-                int cantidad = scanner11.nextInt();
-
-                if (cantidad > 0) {
-                    double suma11 = 0;
-                    double mayor = Double.NEGATIVE_INFINITY;
-                    double menor = Double.POSITIVE_INFINITY;
-
-                    for (int i = 1; i <= cantidad; i++) {
-                        System.out.print("Ingrese el número " + i + ": ");
-                        double numero = scanner11.nextDouble();
-                        suma11 += numero;
-                        if (numero > mayor) {
-                            mayor = numero;
-                        }
-                        if (numero < menor) {
-                            menor = numero;
-                        }
-                    }
-
-                    double media11 = suma11 / cantidad;
-
-                    System.out.println("El mayor número es: " + mayor);
-                    System.out.println("El menor número es: " + menor);
-                    System.out.println("La media de los números es: " + media11);
-                } else {
-                    System.out.println("La cantidad de números debe ser mayor que 0.");
-                }
+                ejercicio11(src);
                 break;
             default:
                 System.out.println("el numero de ejercicio es incorrecto! ");
                 break;
 
 
+        }
+    }
+
+    private static void ejercicio1(Scanner src) {
+        int suma = 0;
+
+        for (int n = 1; n <= 100; n++) {
+            suma += n;
+        }
+        System.out.println("La suma de los números del 1 al 100 es: " + suma);
+
+    }
+
+    private static void ejercicio2(Scanner src) {
+
+        double suma1 = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Ingrese el número " + i + ": ");
+            double numero = src.nextDouble();
+            suma1 += numero;
+        }
+
+        double media = suma1 / 5;
+
+        System.out.println("La media de los cinco números es: " + media);
+
+        src.close();
+    }
+
+    private static void ejercicio3(Scanner src) {
+
+        System.out.print("¿Cuántos números desea introducir? ");
+        int cantidadNumeros = src.nextInt();
+        double suma3 = 0;
+
+        for (int i = 1; i <= cantidadNumeros; i++) {
+            System.out.print("Ingrese el número " + i + ": ");
+            double numero = src.nextDouble();
+            suma3 += numero;
+        }
+
+        double media3 = suma3 / cantidadNumeros;
+
+        System.out.println("La media de los " + cantidadNumeros + " números es: " + media3);
+    }
+
+    private static void ejercicio4(Scanner src) {
+
+        System.out.print("Ingrese el primer número: ");
+        int num1 = src.nextInt();
+        System.out.print("Ingrese el segundo número: ");
+        int num2 = src.nextInt();
+        int sumaPares = 0;
+
+        for (int i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
+            if (i % 2 == 0) {
+                sumaPares += i;
+            }
+        }
+
+        System.out.println("La suma de los números pares entre " + num1 + " y " + num2 + " es: " + sumaPares);
+    }
+
+    private static void ejercicio5(Scanner src) {
+
+        System.out.print("Ingrese el número de términos: ");
+        int numTerms = src.nextInt();
+        double sum = 0.0;
+
+        for (int k = 1; k <= numTerms; k++) {
+            sum += 1.0 / k;
+        }
+
+        System.out.println("La suma de los " + numTerms + " términos es: " + sum);
+    }
+
+    private static void ejercicio6(Scanner src) {
+
+        System.out.print("Ingrese el número de términos: ");
+        int n = src.nextInt();
+        double sumaSerie = 0.0;
+
+        for (int i = 1; i <= n; i++) {
+            sumaSerie += (i * 1.0) / Math.pow(3, i);
+        }
+
+        System.out.println("La suma de los " + n + " términos es: " + sumaSerie);
+    }
+
+    private static void ejercicio7(Scanner src) {
+
+        System.out.print("Ingrese el número de términos: ");
+        int n7 = src.nextInt();
+        double sumaSerie7 = 0.0;
+
+        for (int i = 1; i <= n7; i++) {
+            sumaSerie7 += i / Math.pow(2, i);
+        }
+
+        System.out.println("La suma de los " + n7 + " términos es: " + sumaSerie7);
+    }
+
+    private static void ejercicio8(Scanner src) {
+
+        System.out.println("Desde : ");
+        int Fahrenheit = src.nextInt();
+        System.out.println("Hasta : ");
+        int Fahrenheit1 = src.nextInt();
+        int Celsius = 0;
+        int Kelvin = 0;
+        int Rankine = 0;
+        if (Fahrenheit < Fahrenheit1)
+
+            for (int i = Fahrenheit; i <= Fahrenheit1; i++) {
+                Celsius = (i - 32) * 5 / 9;
+                Kelvin = Celsius + 273;
+                Rankine = i + 459;
+                System.out.println("i: " + i + " Celsius: " + Celsius + " Kelvin: " + Kelvin + " Rankine: " + Rankine);
+            }
+        else if (Fahrenheit > Fahrenheit1) {
+            for (int i = Fahrenheit1; i <= Fahrenheit; i++) {
+                Celsius = (i - 32) * 5 / 9;
+                Kelvin = Celsius + 273;
+                Rankine = i + 459;
+                System.out.println("i: " + i + " Celsius: " + Celsius + " Kelvin: " + Kelvin + " Rankine: " + Rankine);
+            }
+
+        }
+    }
+
+    private static void ejercicio9(Scanner src) {
+
+        System.out.print("Ingrese un número para calcular su factorial: ");
+        int numFactorial = src.nextInt();
+
+        long factorial = 1;
+        for (int i = 1; i <= numFactorial; i++) {
+            factorial *= i;
+        }
+        System.out.println("El factorial de " + numFactorial + " es: " + factorial);
+
+        System.out.println("Factoriales de los primeros 20 números naturales:");
+        for (int i = 0; i <= 20; i++) {
+            long factorial20 = 1;
+            for (int j = 1; j <= i; j++) {
+                factorial20 *= j;
+            }
+            System.out.println(i + "! = " + factorial20);
+        }
+    }
+
+    private static void ejercicio10(Scanner src) {
+
+        System.out.print("Ingrese el número de términos: ");
+        int n10 = src.nextInt();
+
+        int term = 1;
+        for (int i = 1; i <= n10; i++) {
+            System.out.print(term + " ");
+            term *= -2;
+        }
+        System.out.println();
+    }
+
+    private static void ejercicio11(Scanner src) {
+
+        System.out.print("Ingrese la cantidad de números: ");
+        int cantidad = src.nextInt();
+
+        if (cantidad > 0) {
+            double suma11 = 0;
+            double mayor = Double.NEGATIVE_INFINITY;
+            double menor = Double.POSITIVE_INFINITY;
+
+            for (int i = 1; i <= cantidad; i++) {
+                System.out.print("Ingrese el número " + i + ": ");
+                double numero = src.nextDouble();
+                suma11 += numero;
+                if (numero > mayor) {
+                    mayor = numero;
+                }
+                if (numero < menor) {
+                    menor = numero;
+                }
+            }
+
+            double media11 = suma11 / cantidad;
+
+            System.out.println("El mayor número es: " + mayor);
+            System.out.println("El menor número es: " + menor);
+            System.out.println("La media de los números es: " + media11);
+        } else {
+            System.out.println("La cantidad de números debe ser mayor que 0.");
         }
     }
 }
--- PROGRAMACION/src/UD3/Boletin3.java
+++ PROGRAMACION/src/UD3/Boletin3.java
@@ -1,5 +1,24 @@
 package UD3;
 
+import java.time.YearMonth;
+import java.util.Scanner;
+
 public class Boletin3 {
 
+    public static int ejercicio1(int mes, int ano) {
+        YearMonth y = YearMonth.of(ano, mes);
+        int dias = y.lengthOfMonth();
+        return dias;
+    }
+
+    public static void main(String[] args) {
+        Scanner src = new Scanner(System.in);
+        System.out.println("Introduce el mes en formato numérico:");
+        int mes = src.nextInt();
+        System.out.println("Introduce el año:");
+        int ano = src.nextInt();
+        int dias = ejercicio1(mes, ano);
+        System.out.println("El mes " + mes + " del año " + ano + " tiene " + dias + " días.");
+    }
 }
+
