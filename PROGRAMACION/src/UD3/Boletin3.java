f6b6e85061ee3b08c864129c0b8fb8c8e325d94c
Pablo 103 <Pablopereirahospido35@gmail.com>
2024-11-21 21:05:12 +0100

    añadido el try catch

--- PROGRAMACION/src/UD3/Boletin3.java
+++ PROGRAMACION/src/UD3/Boletin3.java
@@ -2,13 +2,21 @@ package UD3;
 
 import java.time.YearMonth;
 import java.util.Scanner;
+import java.lang.Exception;
 
 public class Boletin3 {
 
-    public static int ejercicio1(int mes, int ano) {
-        YearMonth y = YearMonth.of(ano, mes);
-        int dias = y.lengthOfMonth();
-        return dias;
+    public static int ejercicio1(int mes, int anho) {
+        try {
+            YearMonth y = YearMonth.of(anho, mes);
+
+            int dias = y.lengthOfMonth();
+            return dias;
+        } catch (Exception e) {
+            System.out.println("Error: Mes invalido " );
+
+        }
+        return 0;
     }
 
     public static void main(String[] args) {
@@ -16,9 +24,9 @@ public class Boletin3 {
         System.out.println("Introduce el mes en formato numérico:");
         int mes = src.nextInt();
         System.out.println("Introduce el año:");
-        int ano = src.nextInt();
-        int dias = ejercicio1(mes, ano);
-        System.out.println("El mes " + mes + " del año " + ano + " tiene " + dias + " días.");
+        int anho = src.nextInt();
+        int dias = ejercicio1(mes, anho);
+        System.out.println("El mes " + mes + " del año " + anho + " tiene " + dias + " días.");
     }
 }
 
