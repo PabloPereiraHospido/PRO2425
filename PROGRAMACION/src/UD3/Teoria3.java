0693c3d455d5af505378a7ca889bc3f9cc32b114
Pablo 103 <Pablopereirahospido35@gmail.com>
2024-11-25 19:15:32 +0100

    dimos lo que es el stack

--- PROGRAMACION/src/UD3/Teoria3.java
+++ PROGRAMACION/src/UD3/Teoria3.java
@@ -29,6 +29,35 @@ public class Teoria3 {
         System.out.println(Lugar);
     }
 
+    private static void presentar(int edad, String nombre) {
+
+        System.out.println(nombre + "tiene" + edad + "años");
+    }
+
+    private static void presentar() {
+        System.out.println("hola");
+    }
+
+    private static void presentar(String nombre, int edad) {
+        System.out.println(nombre + "tiene" + edad + "años");
+    }
+
+    public class EjemploAmbitos {
+        private int a = 0;
+
+        private void metodo() {
+            int b = 10;
+            for (int i = 0; i < b; i++) {
+                System.out.println(i * b);
+            }
+            metodo2(b);
+        }
+    }
+
+    private static void metodo2(int b) {
+        int resultado = 2 * b;
+    }
+
     public static void main(String[] args) {
         for (int valor = 1; valor <= 2; valor++)
             // imprimirTablaMultiplicar(valor);
@@ -36,6 +65,11 @@ public class Teoria3 {
         System.out.println(hacerMedia(9, 8));
         imprimirPresentacion();
         imprimirPresentacion("Pablo", 24, "Lugo");
+        int a = 10;
+        String b = "Victor";
+        presentar(a, b);
+        presentar();
+        presentar(b, a);
     }
 }
 
