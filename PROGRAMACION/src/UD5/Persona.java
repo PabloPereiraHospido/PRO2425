89651a4d2d76f9b7179b77e1ec8e42b5817b057b
Pablo 103 <Pablopereirahospido35@gmail.com>
2025-01-13 20:01:21 +0100

    empezamos con el tema 5

---
+++ PROGRAMACION/src/UD5/Persona.java
@@ -0,0 +1,12 @@
+package UD5;
+
+
+import java.util.Date;
+
+public class Persona {
+    String nombre;
+    String apellido;
+    Integer edad;
+    String dni;
+    Date fechanacimiento;
+}
---
+++ PROGRAMACION/src/UD5/Teoria5.java
@@ -0,0 +1,16 @@
+package UD5;
+
+public class Teoria5 {
+    public static void main(String[] args) {
+        Persona pablo =new Persona();
+        pablo.edad=25;
+        pablo.apellido="Pereira";
+        pablo.dni="123456.Y";
+        pablo.fechanacimiento=1999-12-9;
+
+        System.out.println(pablo.edad);
+        System.out.println(pablo.apellido);
+        System.out.println(pablo.dni);
+        System.out.println(pablo.fechanacimiento);
+    }
+}
