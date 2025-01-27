7f43445706404199faf53adeeb89f355ce4daa5f
Pablo 103 <Pablopereirahospido35@gmail.com>
2025-01-27 19:38:03 +0100

    repaso tema 3 by victor

---
+++ PROGRAMACION/src/UD3/Recursividad.java
@@ -0,0 +1,26 @@
+package UD3;
+
+public class Recursividad {
+    public static void main(String[] args) {
+        int numero = 5;
+int numero2=12345;
+final int ZERO=0;
+        System.out.println(factorial(5));
+        System.out.println(invertir(numero2,ZERO));
+    }
+
+    private static long factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+    private static int invertir(int n,int invertido){
+        if (n==0){
+            return invertido*10+n;
+        }else {
+            return invertir(n/10,invertido=invertido*10+n%10);
+        }
+    }
+}
